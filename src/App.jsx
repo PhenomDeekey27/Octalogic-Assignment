fcafa6f837128c26bc640372c19ac42a233a9d5f
KasthuriRangan <krangan415@gmail.com>
2024-06-21 17:45:06 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Routes,Route } from 'react-router-dom'
 import Courses from './Pages/Courses'
 import Sidebar from './Components/Sidebar'
 
+
 const App = () => {
   return (
     <div>
   
       <Routes>
         
+
         <Route  path="/" element={<Sidebar></Sidebar>}>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='courses' element={<Courses></Courses>}></Route>
